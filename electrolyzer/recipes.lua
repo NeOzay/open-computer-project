ee3a5bfa9c1db2d3973e4d2d3517fc5607ee0e05
NeOzay <colpaert.benoit@gmail.com>
2021-12-19 16:59:33 +0100

    first

---
+++ electrolyzer/recipes.lua
@@ -0,0 +1,18 @@
+---@shape recipeDust
+---@field amount number
+---@field duration number
+---@field energy number
+---@field name string
+
+local recipe = {}
+
+recipe["Ruby Dust"] = { amount = 6, duration = 29, energy = 60, name = "Ruby Dust" }
+recipe["Red Garnet Dust"] = { amount = 16, duration = 71, energy = 60, name = "Red Garnet Dust" }
+recipe["Sodalite Dust"] = { amount = 11, duration = 53, energy = 60, name = "Sodalite Dust" }
+recipe["Clay Dust"] = { amount = 7, duration = 20, energy = 30, name = "Clay Dust" }
+recipe["Galena Dust"] = { amount = 8, duration = 25, energy = 32, name = "Galena Dust" }
+recipe["Salt"] = { amount = 2, duration = 12, energy = 30, name = "Salt" }
+recipe["Pyrite Dust"] = { amount = 3, duration = 23, energy = 30, name = "Pyrite Dust" }
+recipe["Chalcopyrite Dust"] = { amount = 4, duration = 34, energy = 60, name = "Chalcopyrite Dust" }
+recipe["Bauxite Dust"] = {amount=39,duration=125,energy=60,name="Bauxite Dust"}
+return recipe
