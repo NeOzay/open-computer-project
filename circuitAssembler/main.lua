a6018b4d768b23eeba15577cbeb4f50929148a6f
NeOzay <colpaert.benoit@gmail.com>
2022-06-02 21:19:58 +0000

    pc

--- circuitAssembler/main.lua
+++ circuitAssembler/main.lua
@@ -1,25 +1,16 @@
---local component = require("component")
---local sides =  require("sides")
---local transposer = component.transposer
+local component = require("component")
+local sides =  require("sides")
+local transposer = component.transposer
+local gpu = component.gpu
+local term = require("term")
 
 local recipes = require("recipes")
 
---local assemblerSide = sides.north
---local drawerSide = sides.top
+local assemblerSide = sides.north
+local drawerSide = sides.top
 
---local itemsIterator = transposer.getAllStacks(drawerSide)
-local function NitemsIterator()
-	local n = 0
-	return function ()
-		n = n + 1
-		if n == 7 then
-			return nil
-		end
-		return {label = recipes["Integrated Processor"][n].name}
-	end
-end
+local itemsIterator = transposer.getAllStacks(drawerSide)
 
-local itemsIterator = NitemsIterator()
 ---@return Item[]
 local function selectRecipe()
 	local i = 0
@@ -42,8 +33,8 @@ end
 
 ---@param recipe Item[]
 local function findItems(recipe)
-	--itemsIterator.reset()
-
+	itemsIterator.reset()
+	
 	---@type table<string, number>
 	local itemsSlot = {}
 	---@type {[string]:boolean}
@@ -60,7 +51,7 @@ local function findItems(recipe)
 			itemsSlot[storedItemLabel] = i
 		end
 	end
-
+	
 	local missing = {}
 	local hasmissing = false
 	for name, bool in pairs(toFound) do
@@ -107,8 +98,12 @@ local selectedRecipe, recipeAmount = request()
 local itemsSlot = findItems(selectedRecipe)
 ---@type table<number,number>
 local item_to_transfer = {}
-print("make item_to_transfer")
+
+local stringDecal = 0
+term.clear()
 for index, item in ipairs(selectedRecipe) do
+	stringDecal = math.max(stringDecal, #item.name)
+	gpu.set(1, index, item.name)
 	item_to_transfer[index] = recipeAmount * item.n
 end
 
@@ -118,13 +113,13 @@ end
 ---@return K
 ---@return V
 local function getNext(t, index)
-		local amount
-		index, amount = next(t, index)
-		if not index then
-			index, amount = next(t)
-			print()
-		end
-		return index, amount
+	local amount
+	index, amount = next(t, index)
+	if not index then
+		index, amount = next(t)
+		os.sleep(10)
+	end
+	return index, amount
 end
 
 ---@generic K,V:integer
@@ -134,12 +129,12 @@ end
 ---@return K
 ---@return V
 local function check0(t, index, key)
-		if key and key == 0 then
-			t[index] = nil
-			index, key = getNext(t, index)
-			return check0(t, index, key)
-		end
-		return index, key
+	if key and key == 0 then
+		t[index] = nil
+		index, key = getNext(t, index)
+		return check0(t, index, key)
+	end
+	return index, key
 end
 
 ---@param recipe Item[]
@@ -156,16 +151,24 @@ local function loop(recipe, t2)
 end
 
 
+local function display()
+	for index, amount in pairs(item_to_transfer) do
+		local item = selectedRecipe[index]
+		local storedItem = transposer.getSlotStackSize(drawerSide, itemsSlot[item.name])
+		gpu.set(stringDecal + 5, index, string.format("%.5d", amount))
+		gpu.set(stringDecal + 12, index, string.format("%.5d", storedItem))
+	end
+end
+
 local function main()
 	for index, item, itemAmount in loop(selectedRecipe, item_to_transfer) do
 		local itemStoredSlot = itemsSlot[item.name]
-		--local transfered = transferToAssembler(itemStoredSlot, index, itemAmount)
-		local transfered = 1
-		print(index, item.name, itemAmount)
+		local transfered = transferToAssembler(itemStoredSlot, index, itemAmount)
 		item_to_transfer[index] = itemAmount - transfered
+		display()
 	end
 end
 
 main()
-
+term.clear()
 
