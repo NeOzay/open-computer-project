56cff86e903d1d31337a73f0df8a338fb0ce65e9
NeOzay <colpaert.benoit@gmail.com>
2025-03-04 16:47:30 +0100

    rename

--- RowDisplay/home/example.lua
+++
@@ -1,8 +0,0 @@
-local rowDisplay = require("rowDisplay")
-rowDisplay:clearRows()
-
-local row = rowDisplay:newRow(2)
-row:addCell(3, 10):setText("Hello")
-row:addCell(10):setText("World")
-
-rowDisplay:drawAllRow()
